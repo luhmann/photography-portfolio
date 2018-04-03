7d83afe144459740f583ffc74f650dae6654b0a4
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2018-04-03 17:44:29 +0200

    feat(design): add background styling

---
+++ src/components/Background/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { color, space } from 'styled-system';
+
+const Background = styled.main`
+  ${color};
+  ${space};
+  min-height: 100vh;
+  width: 100vw;
+`;
+
+export default Background;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,35 +1,31 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import { ThemeProvider } from 'styled-components';
+import theme from '../theme';
 
-import Header from '../components/Header'
-import './index.css'
+import Background from '../components/Background';
+import './index.css';
 
 const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
+  <ThemeProvider theme={theme}>
+    <div>
+      <Helmet
+        title="J F Dietrich Photography"
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' },
+        ]}
+      />
+      <Background bg="black" p={1}>
+        {children()}
+      </Background>
     </div>
-  </div>
-)
+  </ThemeProvider>
+);
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
-}
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
