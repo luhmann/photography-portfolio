f698215974a8f0ae49c291f1a34f6c23ee80200e
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2018-04-03 16:07:24 +0200

    feat(images): get images from filesystem via graphql

--- src/templates/album.js
+++ src/templates/album.js
@@ -2,15 +2,42 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import Img from 'gatsby-image';
 
-export default function Template({ pathContext: { images, name } }) {
-  console.log(images, name);
-  const [{ node: { childImageSharp: { sizes: firstImageSizes } } }] = images;
+export default function Template({ data, pathContext: { name } }) {
+  console.log(data, name);
+  const { allFile: { edges: images } } = data;
   return (
     <div className="album-container">
       <Helmet title={`${name} - Album`} />
       <div className="album" style={{ width: '100vw', height: '100vh' }}>
-        <Img sizes={firstImageSizes} />
+        {images.map(image => {
+          const {
+            node: { childImageSharp: { sizes, internal: { contentDigest } } },
+          } = image;
+          return <Img key={contentDigest} sizes={sizes} />;
+        })}
       </div>
     </div>
   );
 }
+
+export const pageQuery = graphql`
+  query ImagesByPath($name: String!) {
+    allFile(
+      filter: { relativeDirectory: { eq: $name } }
+      sort: { fields: [relativePath] }
+    ) {
+      edges {
+        node {
+          childImageSharp {
+            internal {
+              contentDigest
+            }
+            sizes(maxWidth: 2500) {
+              ...GatsbyImageSharpSizes_withWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`;
