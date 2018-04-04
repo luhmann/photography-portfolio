6bf05c78731132d6706918e69266be9ff1e66298
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2018-04-04 17:41:01 +0200

    feat(menu): add barebone menu

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Background } from './Background';
 export { default as Logo } from './Logo';
+export { default as Menu } from './Menu';
 export { default as StyledLink } from './StyledLink';
