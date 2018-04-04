12e1dc75ac180c7c14c4add594948284251e30fe
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2018-04-04 16:05:26 +0200

    feat(logo): add logo

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,13 +1,21 @@
 import styled from 'styled-components';
-import { color, space } from 'styled-system';
+import { color, fontFamily, fontSize, space, themeGet } from 'styled-system';
 
 const Logo = styled.h1`
   ${color};
   ${space};
+  ${fontFamily};
+  ${fontSize};
+  position: absolute;
+  top: ${themeGet('space.3')}px;
+  left: ${themeGet('space.3')}px;
+  z-index: ${themeGet('zIndex.high')};
 `;
 
 Logo.propTypes = {
   ...color.propTypes,
+  ...fontFamily.propTypes,
+  ...fontSize.propTypes,
   ...space.propTypes,
 };
 
--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,48 @@
 const colors = {
   black: '#22292f',
-  grey: {},
+  grey: {
+    darkest: '#3d4852',
+    darker: '#606f7b',
+    dark: '#8795a1',
+    base: '#b8c2cc',
+    light: '#dae1e7',
+    lighter: '#f1f5f8',
+    lightest: '#f8fafc',
+  },
   white: '#fff',
+};
+
+const fonts = {
+  logo: '"Fjalla One", sans-serif',
+};
+
+const fontSizes = {
+  xs: '.75rem', // 12px
+  sm: '.875rem', // 14px
+  base: '1rem', // 16px
+  lg: '1.125rem', // 18px
+  xl: '1.25rem', // 20px
+  '2xl': '1.5rem', // 24px
+  '3xl': '1.875rem', // 30px
+  '4xl': '2.25rem', // 36px
+  '5xl': '3rem', // 48px
+};
+
+// Spacing Scale (used for margin and padding)
+const space = [0, 4, 8, 16, 32, 64, 128, 256, 512];
+
+const zIndex = {
+  low: 10,
+  middle: 50,
+  high: 100,
 };
 
 const theme = {
   colors,
+  fonts,
+  fontSizes,
+  space,
+  zIndex,
 };
 
 export default theme;
