3c595a97cdd296053a6121f920e209c50b569a39
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2018-04-05 14:34:45 +0200

    feat(menu): apply first iteration of design to menu

--- src/components/StyledLink/index.js
+++ src/components/StyledLink/index.js
@@ -1,19 +1,37 @@
 import styled from 'styled-components';
 import Link from 'gatsby-link';
-import { color, themeGet } from 'styled-system';
+import {
+  color,
+  display,
+  fontFamily,
+  fontSize,
+  fontWeight,
+  space,
+  themeGet,
+} from 'styled-system';
 
 const StyledLink = styled(Link)`
   ${color};
+  ${fontFamily};
+  ${fontSize};
+  ${fontWeight};
+  ${display};
+  ${space};
   text-decoration: none;
 
   &:hover,
   &:hover > * {
-    color: ${themeGet('colors.grey.darkest')};
+    color: ${themeGet('colors.red.dark')};
   }
 `;
 
 StyledLink.propTypes = {
   ...color.propTypes,
+  ...display.propTypes,
+  ...fontFamily.propTypes,
+  ...fontSize.propTypes,
+  ...fontWeight.propTypes,
+  ...space.propTypes,
 };
 
 StyledLink.displayName = 'StyledLink';
