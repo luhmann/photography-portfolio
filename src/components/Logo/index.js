9447c960f57b26530093078840e3c745003ff1d0
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2018-04-05 21:52:21 +0200

    refactor(logo): make logo copy static, make color configurable

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,20 +1,25 @@
+import React from 'react';
 import styled from 'styled-components';
-import { space, themeGet } from 'styled-system';
+import { space, color, themeGet } from 'styled-system';
 
-const Logo = styled.h1`
+const LogoStyle = styled.h1`
   ${space};
-  color: ${themeGet('colors.black')};
+  ${color};
   font-family: ${themeGet('fonts.logo')};
   font-size: ${themeGet('fontSizes.xl')};
   font-weight: ${themeGet('fontWeights.bold')};
   position: absolute;
   top: ${themeGet('space.3')};
   left: ${themeGet('space.4')};
+  margin: ${themeGet('space.0')};
   z-index: ${themeGet('zIndex.high')};
 `;
 
+const Logo = props => <LogoStyle {...props}>JF Dietrich Photography</LogoStyle>;
+
 Logo.propTypes = {
   ...space.propTypes,
+  ...color.propTypes,
 };
 
 Logo.displayName = 'Logo';
