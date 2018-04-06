ac78408d0ee652e9c3f7066799d26726726449bb
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2018-04-06 12:08:12 +0200

    feat(design): add basic responsive styles to index-page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,10 @@ import Img from 'gatsby-image';
 import Link from 'gatsby-link';
 import { ifElse, inc } from 'rambda';
 import styled, { css } from 'styled-components';
+import { space, themeGet } from 'styled-system';
 import { mapGalleryImagesGraphQLResponse } from '../utils/mappings';
 import { ContentContainer, Logo } from '../components';
-import { themeGet } from 'styled-system';
+import { media } from '../theme';
 
 const Image = styled.div`
   height: 100%;
@@ -28,12 +29,17 @@ const IndexLogo = styled(Logo)`
   font-size: ${themeGet('fontSizes.4xl')};
   text-shadow: rgba(0, 0, 0, 0.5) 0 0 10px;
   position: static;
+
+  ${media.sm`
+    font-size: ${themeGet('fontSizes.3xl')};
+    line-height: ${themeGet('lineHeights.tight')};
+  `};
 `;
 
 const PortfolioButton = styled(Link)`
   background-color: rgba(0, 0, 0, 0.05);
   border: 2px solid ${themeGet('colors.white')};
-  display: block;
+  display: inline-block;
   font-family: 'Helvetica Neue', Helvetica, Arial, sans-serf;
   font-size: ${themeGet('fontSizes.sm')};
   color: ${themeGet('colors.white')};
@@ -48,6 +54,10 @@ const PortfolioButton = styled(Link)`
     background-color: ${themeGet('colors.white')};
     color: #000;
   }
+
+  ${media.sm`
+    padding: ${themeGet('space.3')} ${themeGet('space.3')};
+  `};
 `;
 
 const Footer = styled.div`
@@ -57,30 +67,37 @@ const Footer = styled.div`
   bottom: 0;
   height: 15vh;
   width: 100%;
-  padding: ${themeGet('space.0')} ${themeGet('space.6')};
+  padding: ${themeGet('space.2')} ${themeGet('space.6')};
   justify-content: space-between;
+
+  ${media.sm`
+    display: block;
+    height: 30vh;
+    padding: ${themeGet('space.2')} ${themeGet('space.4')};
+  `};
+
+  ${media.md`
+    padding: ${themeGet('space.2')} ${themeGet('space.4')};
+  `};
 `;
 
 let intervalId;
 const clearSideshowInterval = ({ intervalId }) =>
   window.clearTimeout(intervalId);
 
-const IndexPage = ({ images, imageIndex, next }) => {
-  console.log(images, imageIndex, next);
-  return (
-    <ContentContainer>
-      {images.map((image, index) => (
-        <Image key={image.contentDigest} invisible={index !== imageIndex}>
-          <Img sizes={image.sizes} style={{ height: '100%' }} />
-        </Image>
-      ))}
-      <Footer>
-        <IndexLogo color="white" />
-        <PortfolioButton to="/norway">Portfolio</PortfolioButton>
-      </Footer>
-    </ContentContainer>
-  );
-};
+const IndexPage = ({ images, imageIndex }) => (
+  <ContentContainer>
+    {images.map((image, index) => (
+      <Image key={image.contentDigest} invisible={index !== imageIndex}>
+        <Img sizes={image.sizes} style={{ height: '100%' }} />
+      </Image>
+    ))}
+    <Footer>
+      <IndexLogo color="white" mb={[5, 0]} />
+      <PortfolioButton to="/norway">Portfolio</PortfolioButton>
+    </Footer>
+  </ContentContainer>
+);
 
 IndexPage.propTypes = {
   images: PropTypes.arrayOf(
@@ -90,7 +107,6 @@ IndexPage.propTypes = {
     })
   ).isRequired,
   imageIndex: PropTypes.number.isRequired,
-  next: PropTypes.func.isRequired,
 };
 
 export default compose(
